6d7443be982cffe8edba1a9acdc1aaa26a2de41e
KristineVog <33103662+kristinevog@users.noreply.github.com>
2024-02-23 22:20:13 +0000

    changed

--- tools/img-web.sh
+++ tools/img-web.sh
@@ -3,9 +3,9 @@
 echo "Enter the filename (with extension), the new image name and extension - "
 read image
 imgfile="$image"
-newname=$2
-newimg=$2.$3
-newext=$3
+newname='banner'
+newext='jpg'
+newimg=$newname.$newext
 echo "$imgfile $newimg"
 convert $imgfile -sampling-factor 4:2:0 -strip -quality 85   -interlace Plane -colorspace RGB $newimg
 convert -resize 5% $newimg $newname-thumb.$newext
