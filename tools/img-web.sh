7a4e23480c41a7011aa073673c4b1fa0dae3332e
KristineVog <33103662+kristinevog@users.noreply.github.com>
2024-02-23 22:35:28 +0000

    changed path

--- tools/img-web.sh
+++ tools/img-web.sh
@@ -2,10 +2,11 @@
 #  -gaussian-blur 0.05
 echo "Enter the filename (with extension), the new image name and extension - "
 read image
-imgfile="$image"
+read location ## assets/images/posts/2024-02-23-Pulumi-end-to-end-on-Azure
+imgfile="$location"+"/"+"$image"
 newname='banner'
 newext='jpg'
-newimg=$newname.$newext
+newimg="$location"+$newname.$newext
 echo "$imgfile $newimg"
 convert $imgfile -sampling-factor 4:2:0 -strip -quality 85   -interlace Plane -colorspace RGB $newimg
 convert -resize 5% $newimg $newname-thumb.$newext
