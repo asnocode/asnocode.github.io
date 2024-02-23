5c8c87b232a027d7e22aa71920b676f2fe75235b
KristineVog <33103662+kristinevog@users.noreply.github.com>
2024-02-23 22:44:47 +0000

    change

--- tools/img-web.sh
+++ tools/img-web.sh
@@ -1,12 +1,13 @@
 # Compresses images for web, creates a thumbnail and a webp file.
 #  -gaussian-blur 0.05
 echo "Enter the filename (with extension), the new image name and extension - "
-read image
+read image ## banner.jpg
+echo "Enter the filename (with extension), the new image name and extension - "
 read location ## assets/images/posts/2024-02-23-Pulumi-end-to-end-on-Azure
-imgfile="$location"+"/$image"
+imgfile="$location/$image"
 newname='banner'
 newext='jpg'
-newimg="$location"+$newname.$newext
+newimg="$location/"+$newname.$newext
 echo "$imgfile $newimg"
 convert $imgfile -sampling-factor 4:2:0 -strip -quality 85   -interlace Plane -colorspace RGB $newimg
 convert -resize 5% $newimg $newname-thumb.$newext
